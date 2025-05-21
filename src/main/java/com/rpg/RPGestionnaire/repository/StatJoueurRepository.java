8bb982198328dabab4f6f85f37d45e95f17cc0f5
Krolaf <135095887+Krolaf@users.noreply.github.com>
2025-05-21 16:09:31 +0400

    admin_tats_remove

--- src/main/java/com/rpg/RPGestionnaire/repository/StatJoueurRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rpg.RPGestionnaire.repository;
-
-import com.rpg.RPGestionnaire.entity.StatJoueur;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface StatJoueurRepository extends JpaRepository<StatJoueur, UUID> {
-} 
